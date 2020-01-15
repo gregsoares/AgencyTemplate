e92cc44ba2cb76d36add9502e669163fe6f96203
Greg Soares <gregselvagem@gmail.com>
2020-01-14 23:14:21 -0500

    fixed dual Hero

--- client/src/App.js
+++ client/src/App.js
@@ -28,26 +28,6 @@ class App extends Component {
                 // { btnThree: windowsBtn }
           />
 
-
-
-  {/* <!--========================== */}
-    {/* Hero Section */}
-  {/* ============================--> */}
-  <section id="hero" className="wow fadeIn">
-    <div className="hero-container">
-      <h1>Welcome to eStartups</h1>
-      <h2>Elegant Bootstrap Template for Startups, Apps &amp; more...</h2>
-      <img src="img/hero-img.png" alt="Hero Imgs" />
-      <a href="/get-started" className="btn-get-started scrollto">Get Started</a>
-      <div className="btns">
-        <a href="/"><i className="fa fa-apple fa-3x"></i> App Store</a>
-        <a href="/"><i className="fa fa-play fa-3x"></i> Google Play</a>
-        <a href="/"><i className="fa fa-windows fa-3x"></i> windows</a>
-      </div>
-    </div>
-  </section>
-  {/* <!-- #hero --> */}
-
   {/* <!--========================== */}
     {/* Get Started Section */}
   {/* ============================--> */}
