b2042ae6d83f4b51dc15be5645450d00109ec95b
Yggdrasil <gregselvagem@gmail.com>
2020-08-29 23:20:30 -0400

    cleanup, firebase ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,59 @@
-import React, { Component } from 'react';
-// import logo from './logo.svg';
+import React, { Component } from "react";
 import AboutUs from "./Components/AboutUs/AboutUs";
-import Blog from './Components/Blog/Blog';
-import Contact from './Components/Contact/Contact';
-import Features from './Components/Features/Features';
-import Footer from './Components/Footer/Footer';
-import GetStarted from './Components/GetStarted/GetStarted';
-import Newsletter from './Components/Newsletter/Newsletter';
-import PriceTable from './Components/PriceTable/PriceTable';
-import ScreenShots from './Components/ScreenShots/ScreenShots';
-import Team from './Components/Team/Team';
-import Testimonials from './Components/Testimonials/Testimonials';
-import TopHeader from './Components/TopHeader/TopHeader';
-import TopHero from './Components/TopHero/TopHero';
-import Video from './Components/Video/Video';
+import Blog from "./Components/Blog/Blog";
+import Contact from "./Components/Contact/Contact";
+import Features from "./Components/Features/Features";
+import Footer from "./Components/Footer/Footer";
+import GetStarted from "./Components/GetStarted/GetStarted";
+import Newsletter from "./Components/Newsletter/Newsletter";
+import PriceTable from "./Components/PriceTable/PriceTable";
+import ScreenShots from "./Components/ScreenShots/ScreenShots";
+import Team from "./Components/Team/Team";
+import Testimonials from "./Components/Testimonials/Testimonials";
+import TopHeader from "./Components/TopHeader/TopHeader";
+import TopHero from "./Components/TopHero/TopHero";
+import Video from "./Components/Video/Video";
 
 class App extends Component {
-
-
   render() {
-
     const icons = {
       iconOne: {
-        text: 'Icon One Text'
+        text: "Icon One Text",
       },
       iconTwo: {
-        text: 'Icon Two Text'
+        text: "Icon Two Text",
       },
       iconThree: {
-        text: 'Icon Three Text'
+        text: "Icon Three Text",
       },
-    }
-    const testimonials = {
-      testimonial: {
-        title: 'T1',
-        text: 'Such nice things to say',
-        authorName: 'Greg Soares',
-        authorPosition: 'Boss'
-      }
-    }
+    };
+
+    const testimonials = [
+      {
+        title: "Works as expected",
+        text:
+          " Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+        authorName: "Greg Soares",
+        authorPosition: "User",
+        key: 0,
+      },
+      {
+        title: "Testimonial Two",
+        text:
+          "Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+        authorName: "Greg Soares",
+        authorPosition: "Dev guy",
+        key: 1,
+      },
+      {
+        title: "T1",
+        text:
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.",
+        authorName: "Thoman Henderson",
+        authorPosition: "Manager",
+        key: 2,
+      },
+    ];
 
     return (
       <React.Fragment>
@@ -46,15 +61,15 @@ class App extends Component {
 
         <TopHero
           title="Company Intro Section"
-          subtitle='Section Subtitle here'
-          imgPath='img/hero-img.png'
-          btnText='Explore'
+          subtitle="Section Subtitle here"
+          imgPath="img/hero-img.png"
+          btnText="Explore"
           iconOne={icons.iconOne}
           iconTwo={icons.iconTwo}
           iconThree={icons.iconThree}
         />
         <Features />
-        <Testimonials content={testimonials.testimonial}/>
+        <Testimonials content={testimonials} />
         <AboutUs />
         <Team />
         <Video />
@@ -66,7 +81,9 @@ class App extends Component {
         <Contact />
         <Footer />
 
-        <a href="/" className="back-to-top"><i className="fa fa-chevron-up"></i></a>
+        <a href="/" className="back-to-top">
+          <i className="fa fa-chevron-up"></i>
+        </a>
 
         {/* <!-- JavaScript Libraries --> */}
         <script src="lib/jquery/jquery.min.js"></script>
@@ -83,9 +100,7 @@ class App extends Component {
 
         {/* <!-- Template Main Javascript File --> */}
         <script src="js/main.js"></script>
-
       </React.Fragment>
-
     );
   }
 }
--- client/src/Components/Testimonials/Testimonials.js
+++ client/src/Components/Testimonials/Testimonials.js
@@ -1,84 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Testimonials = props => {
+const Testimonials = (props) => {
+  const content = props.content;
+  const [slide, setSlide] = useState([]);
 
-  // TODO: on componentDidMount load all testimonials
+  // FIXME: CarouselItem not taking in input, testimonials not loading!
+  const carouselItem = () => {
+    content.map((testimonial) =>
+      testimonial ? (
+        <div className="carousel-item active " key={testimonial.key}>
+          <div className="top-top">
+            <h2>{testimonial.title}</h2>
+            <p>{testimonial.text}</p>
+            <h4>
+              {testimonial.authorName}
+              <span>{testimonial.authorPosition} </span>
+            </h4>
+          </div>
+        </div>
+      ) : (
+        "No data received into carouselItem()"
+      )
+    );
+  };
+  const loadIndicators = () => {
+    let indicators = content.map((testimonial) => testimonial.key);
+    return indicators;
+  };
+  useEffect(() => {
+    const target = document.getElementById("testimonialContent");
+    if (content.length !== 0) content && setSlide(carouselItem);
+  }, []);
 
-  const carouselItem = testimonial => { return (
-    <div className="carousel-item active ">
-      <div className="top-top">
-
-        <h2>{testimonial.title}</h2>
-        <p>{testimonial.text}</p>
-        <h4>{testimonial.authorName}<span>{testimonial.authorPosition} </span></h4>
-
-      </div>
-    </div>
-  )}
-
-  // FIXME: onClick() make change to className to add/remove "active" from carousel-item div and <li data-target> 
   return (
     <React.Fragment>
       {/* <!--========================== */}
       {/* Testimonials Section */}
       {/* ============================--> */}
 
-      <section id="testimonials" className="padd-section text-center wow fadeInUp">
+      <section
+        className="padd-section text-center wow fadeInUp"
+        id="testimonials"
+      >
         <div className="container">
           <div className="row justify-content-center">
-
             <div className="col-md-10">
-
               <div className="testimonials-content">
-                <div id="carousel-example-generic" className="carousel slide" data-ride="carousel">
-
-                  <div className="carousel-inner" role="listbox">
-
-                  {carouselItem(props.content)}
-
-                    {/* <div className="carousel-item">
-                      <div className="top-top">
-
-                        <h2>Second Testimonial</h2>
-                        <p>Simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
-                      specimen book. It has survived not only five centuries.</p>
-                        <h4>Henderson<span>manager</span></h4>
-
-                      </div>
-                    </div>
-
-                    <div className="carousel-item active">
-                      <div className="top-top">
-
-                        <h2>Volumes us</h2>
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type
-                      specimen book. It has survived not only five centuries.</p>
-                        <h4>David Spark<span>manager</span></h4>
-
-                      </div>
-                    </div> */}
-
-                  </div>
+                <div
+                  id="carousel-example-generic"
+                  className="carousel slide"
+                  data-ride="carousel"
+                >
+                  <div
+                    className="carousel-inner"
+                    role="listbox"
+                    id="testimonialContent"
+                  ></div>
 
                   <div className="btm-btm">
-
                     <ul className="list-unstyled carousel-indicators">
-                      <li data-target="#carousel-example-generic" data-slide-to="0" className="active"></li>
-                      <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                      <li data-target="#carousel-example-generic" data-slide-to="2" ></li>
+                      {/* load testimonials here */}
                     </ul>
-
                   </div>
-
                 </div>
               </div>
             </div>
-
           </div>
         </div>
       </section>
-
     </React.Fragment>
-  )
-}
+  );
+};
 export default Testimonials;
